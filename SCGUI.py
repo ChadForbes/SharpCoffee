0ce39293734686a4c4130ce928e9340071e20a4c
feral <alleycatsmejkal@gmail.com>
2020-01-16 15:03:27 -0500

    Add files via upload

--- SCGUI.py
+++ SCGUI.py
@@ -11,8 +11,12 @@ def exportAction():
 def importAction():
     pass
 
-#TODO: send the translated Java code to a file
-def SaveAction():
+#TODO: send code to existing file
+def SaveAction(Language):
+    pass
+
+#TODO: send code to new file
+def SaveAsAction(Language):
     pass
 
 #begining UI setup
@@ -29,6 +33,13 @@ CSharpTextArea.grid(row =1,column=0, padx=10, pady=10)
 ImportButton = Button(parentWindow,text = "import", command = importAction)
 ImportButton.grid(row=3,column=0, padx=10, pady=10)
 
+CSharpSaveFrame = tkinter.Frame(parentWindow)
+CSharpSaveFrame.grid(row=4,column=0)#removed padding to make it look nicer imo
+SaveCSharpButton = Button(CSharpSaveFrame, text="save", command = SaveAction("CSharp"))
+SaveCSharpButton.grid(row=0,column=0, padx=10, pady=10)
+SaveAsCSharpButton = Button(CSharpSaveFrame, text="save as", command = SaveAsAction("CSharp"))
+SaveAsCSharpButton.grid(row=0,column=1, padx=10, pady=10)
+
 ExportButton = Button(parentWindow,text = "export", command = exportAction)
 ExportButton.grid(row=3,column=2, padx=10, pady=10)
 
@@ -38,8 +49,12 @@ JavaLabel.grid(row=0, column=3, padx=10, pady=10)
 JavaTextArea = scrolledtext.ScrolledText(parentWindow,width=50,height=20)
 JavaTextArea.grid(row =1,column=3, padx=10, pady=10)
 
-SaveButton = Button(parentWindow,text = "save", command = SaveAction)
-SaveButton.grid(row=3,column=3, padx=10, pady=10)
+JavaSaveFrame = tkinter.Frame(parentWindow)
+JavaSaveFrame.grid(row=3,column=3, padx=10, pady=10)
+SaveJavaButton = Button(JavaSaveFrame, text ="save", command = SaveAction("Java"))
+SaveJavaButton.grid(row=0,column=0, padx=10, pady=10)
+SaveAsJavaButton = Button(JavaSaveFrame, text ="save as", command = SaveAsAction("Java"))
+SaveAsJavaButton.grid(row=0,column=1, padx=10, pady=10)
 
 #this makes sure the GUI stays up
 parentWindow.mainloop()
