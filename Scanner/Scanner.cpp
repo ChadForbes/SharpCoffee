520825190530120a9ecd1dd2587ef44fda716793
Glenden Johnson <glendenj@yahoo.com>
2020-01-16 21:46:11 -0500

    Implemented some of the basic framework of the Scanner.

--- Scanner/Scanner.cpp
+++ Scanner/Scanner.cpp
@@ -1,1 +1,44 @@
+#include <stdlib.h>
+#include <vector>
+#include <tuple>
 #include "Scanner.h"
+
+using namespace std;
+
+unsigned int Scanner::GetInputCode()
+{
+	if (m_InputFile.eof())
+	{
+		return 1;
+	}
+	for (int k = 0; k < m_CharSet.size(); ++k)
+	{
+		if (m_CurrentChar >= get<0>(m_CharSet[k]) && m_CurrentChar <= get<1>(m_CharSet[k]))
+		{
+			return get<2>(m_CharSet[k]);
+		}
+	}
+
+	return 0;
+}
+
+int Scanner::NextLexeme()
+{
+	
+	unsigned int z_Automaton = 0;
+	unsigned int z_CurrentState = 0;
+	unsigned int z_InputCode = 0;
+	while (true)
+	{
+		z_InputCode = GetInputCode();
+
+		z_CurrentState = m_Automata[z_Automaton][z_CurrentState][z_InputCode];
+
+		switch (z_CurrentState)
+		{
+
+		default:
+			continue;
+		}
+	}
+}
--- Scanner/Scanner.h
+++ Scanner/Scanner.h
@@ -1,5 +1,32 @@
 #pragma once
+#include <string>
+#include <fstream>
+
 class Scanner
 {
+public:
+	Scanner(std::string a_FilePath);
+	Scanner() {};
+	~Scanner()
+	{
+		m_InputFile.close();
+	}
+
+	std::ifstream m_InputFile;
+	std::string m_CurrentLexeme = "";
+
+	int NextLexeme();
+	int TokenCode();
+
+private:
+	char m_CurrentChar;
+	std::vector<std::vector<std::vector<unsigned int>>> m_Automata;
+	std::vector<std::tuple<int, int, int>> m_CharSet;
+
+	unsigned int GetInputCode();
+
+	//Disallow copying or assignment.
+	Scanner(Scanner& a_Source) {};
+	Scanner operator =(Scanner& a_Value) {};
 };
 
