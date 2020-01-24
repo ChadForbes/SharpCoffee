d1402174e8f6e7e6915e979e80af4e272cf13514
otran9 <36254277+otran9@users.noreply.github.com>
2020-01-23 23:01:38 -0500

    Add files via upload

---
+++ TestPrograms/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class HelloWorld
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
+
+/*
+ * Keywords:
+ * using, namespace,
+ * class, static, void, Main
+ * Console.WriteLine();
+ */
