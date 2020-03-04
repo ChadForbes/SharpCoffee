d7dcc2b3122d8e2c3e11529fdb6b00cebf6ec1d0
ZEUS\chads <ninjagamer32@gmail.com>
2020-03-04 15:13:47 -0500

    Fixed the parsing library
    Currently parses the C#_to_Java_Mapping.json

--- JsonParser/JsonParser/Source.cpp
+++ JsonParser/JsonParser/Source.cpp
@@ -1,25 +1,58 @@
-#include "filereadstream.h"
-#include "document.h"
-#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <json\json.h>// or jsoncpp/json.h , or json/json.h etc.
 
-using namespace rapidjson;
+using namespace std;
 
-int main() {
+map<string, map<string, string>> Getcpptojavaparse() {
+	Json::Reader reader;
+	Json::Value root;
+	ifstream ifs("C#_to_Java_Mapping.json");
+	reader.parse(ifs, root, false); // reader can also read strings
+	Json::Value::Members popnam = root.getMemberNames();
+	Json::Value::Members membername;
+	//map<string, string> z_innermap;
+	map<string, map<string, string>> z_maps;
+	string v1;
+	int i = 0;
+	for (string x : popnam) {
+		membername = root[x].getMemberNames();
+		z_maps.insert(make_pair(x, map<string, string>()));
+		for (string y : membername) {
+			z_maps[x].insert(make_pair(y, root[x][y].asString()));
+			//cout <<  << endl;
 
-	FILE* fp = fopen("test.json", "r"); // non-Windows use "r"
+		}
+		i++;
 
-	char readBuffer[65536];
-	FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+	}
 
-	Document d;
-	d = d.ParseStream(is);
+	map<string, map<string, string>>::iterator itr;
+	map<string, string>::iterator ptr;
 
-	assert(d.IsObject());
-	assert(d.HasMember("hello"));
-	assert(d["hello"].IsString());
-	printf("hello = %s\n", d["hello"].GetString());
-	fclose(fp);
+	for (itr = z_maps.begin(); itr != z_maps.end(); itr++) {
+  
+        for (ptr = itr->second.begin(); ptr != itr->second.end(); ptr++) { 
+            cout << "First key is '" << itr->first 
+                 << "' And second key is '" << ptr->first 
+                 << "' And value is '" << ptr->second << "'"<< endl; 
+        } 
+    } 
+	cout << "Objects parsed: " << i << endl;
+
+	return z_maps;
+}
+
+
+
+
+
+
+
+int main(){
+
+	Getcpptojavaparse();
 
 	return 0;
-
 }
